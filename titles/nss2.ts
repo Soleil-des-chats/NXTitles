e5c5cc44c269f5c7b54f802d821679d1bedde3fb
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 01:06:39 +0200

    Updated nss2.ts
    Updated to json (Still .ts for now but not for long maybe)

--- titles/nss2.ts
+++ titles/nss2.ts
@@ -1,11 +1,9 @@
-import { Title } from '../types.js';
-
-export const titles: Title[] = [
+[
     {
         "id": "0400c3f00006e000",
         "titleName": "Mario Kart World",
         "client": "1402421197144330380",
         "showPlayingOnline": true,
         "showActiveEvent": true
-    },
-];
+    }
+]
