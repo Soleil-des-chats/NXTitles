bbda0434246f9ce62d8a060493a443b788da5293
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 01:20:48 +0200

    Update nss2.ts
    I don't even know anymore man

--- titles/nss2.ts
+++ titles/nss2.ts
@@ -1,9 +1,8 @@
-[
+export const titles: Title[] = [
     {
-        "id": "0400c3f00006e000",
-        "titleName": "Mario Kart World",
-        "client": "1402421197144330380",
-        "showPlayingOnline": true,
-        "showActiveEvent": true
-    }
-]
+        id: '0400c3f00006e000',
+        client: '1402421197144330380',
+        showPlayingOnline: true,
+        showActiveEvent: true,
+    },
+];
