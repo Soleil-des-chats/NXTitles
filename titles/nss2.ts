8ef53fdb2f11e438345beb7e93342dd141af123f
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 01:01:06 +0200

    Updated nss2.ts
    Updated to use new parsing system.

--- titles/nss2.ts
+++ titles/nss2.ts
@@ -2,10 +2,10 @@ import { Title } from '../types.js';
 
 export const titles: Title[] = [
     {
-        // Mario Kart World
-        id: '0400c3f00006e000',
-        client: '1402421197144330380',
-        showPlayingOnline: true,
-        showActiveEvent: true,
+        "id": "0400c3f00006e000",
+        "titleName": "Mario Kart World",
+        "client": "1402421197144330380",
+        "showPlayingOnline": true,
+        "showActiveEvent": true
     },
 ];
