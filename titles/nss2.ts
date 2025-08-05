be19e270abb11dd8addcd5619db5a228a465655e
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 00:43:16 +0200

    Create nss2.ts

---
+++ titles/nss2.ts
@@ -0,0 +1,11 @@
+import { Title } from '../types.js';
+
+export const titles: Title[] = [
+    {
+        // Mario Kart World
+        id: '0400c3f00006e000',
+        client: '1402421197144330380',
+        showPlayingOnline: true,
+        showActiveEvent: true,
+    },
+];
