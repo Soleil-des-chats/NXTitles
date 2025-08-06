f05d39899f80b04e36e71be7502c087f3e133646
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 16:30:31 +0200

    Created gungho.ts

---
+++ titles/gungho.ts
@@ -0,0 +1,8 @@
+export const titles: Title[] = [
+    {
+        id: '0100CCD0073EA000',
+        client: '1402659206880034886',
+        showPlayingOnline: true,
+        showActiveEvent: true,
+    },
+];
