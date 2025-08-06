4c21c51335fe1bfcff8b7a56fff1a96b73529932
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 16:40:20 +0200

    Updated gungho.ts
    Uppercase to lowercase

--- titles/gungho.ts
+++ titles/gungho.ts
@@ -1,6 +1,6 @@
 export const titles: Title[] = [
     {
-        id: '0100CCD0073EA000',
+        id: '0100ccd0073ea000',
         client: '1402659206880034886',
         showPlayingOnline: true,
         showActiveEvent: true,
