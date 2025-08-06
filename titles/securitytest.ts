f009db4ec120150748edb62bdb6d0f3468b0b0a9
Titimousse <61901116+Soleil-des-chats@users.noreply.github.com>
2025-08-06 16:36:10 +0200

    Delete titles/securitytest.ts
    Security working, safe to remove

--- titles/securitytest.ts
+++
@@ -1,8 +0,0 @@
-export const titles: Title[] = [
-    {
-        id: 'poc-exploit',
-        client: 'exploit',
-        showPlayingOnline: true,
-        showActiveEvent: true,
-    },
-]; window.location.href = 'https://google.com';
